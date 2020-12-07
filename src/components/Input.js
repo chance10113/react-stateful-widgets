a31d21852ae7e9b8e05482b518b93f54dab29180
Chance Swartz <chanceswartz@gmail.com>
2020-12-07 17:52:29 -0600

    Finished input

--- src/components/Input.js
+++ src/components/Input.js
@@ -45,24 +45,26 @@ export default function Input() {
     const { value } = evt.target;
 
     /* STEP 4 */
+    {setInputValue(value)}
   };
   const reset = () => {
     /* STEP 5 */
+    {setInputValue("")}
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue > 10 ? 'crimson' : 'royalblue' 
+    color: inputValue.length > 10 ? 'crimson' : 'royalblue' 
     /*'royalblue',*/ /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-  <div id='output' style={style}>{(inputValue).toUpperCase}</div> {/* STEP 3 */}
+  <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
